802f286d50061d254a7f24f6ba5287602e01cc81
Miroslav Savel <miroslav.savel@gmail.com>
2022-03-16 15:30:04 +0100

    single neuron with numpy

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    inputs = [1.0, 2.0, 3.0, 2.5]
+    weights = [0.2, 0.8, -0.5, 1.0]
+    bias = 2.0
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    outputs = np.dot(weights, inputs) + bias
+    print(outputs)
+
+
